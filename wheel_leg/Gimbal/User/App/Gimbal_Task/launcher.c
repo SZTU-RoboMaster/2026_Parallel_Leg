c73309099fd0f4afa08117c5a51eff38761bf82d
陈开烁 <3455215578@qq.com>
2025-11-22 01:18:28 +0800

    上一次提交没解决单发问题，改了ABS就好了????!!!!

--- wheel_leg/Gimbal/User/App/Gimbal_Task/launcher.c
+++ wheel_leg/Gimbal/User/App/Gimbal_Task/launcher.c
@@ -127,6 +127,9 @@ int8_t vision_single = 0; // 0为不开火，1为视觉单发模式，2为视觉无限连发模式
 /** Shoot **/
 bool single_shoot_finish = true; // 确保打完当前一发，才能执行下一次单发任务
 
+bool test = false;
+int32_t error_test = 0;
+
 /** 拨盘控制 **/
 int16_t count = 0;
 static void trigger_control(void)
@@ -195,31 +198,38 @@ static void trigger_control(void)
             {
                 if(!stop_flag) // 不停火
                 {
-                    if(vision_single == 1) // 视觉单发
+                    if(robot_ctrl.fire_command == 1) // 视觉单发
                     {
                         launcher.trigger_mode = TRIGGER_SINGLE;
 
-                        launcher.trigger.target_total_ecd = launcher.trigger.motor_measure.total_ecd;
+                        if(single_shoot_finish)
+                        {
+                            single_shoot_finish = false;
 
-                        launcher.trigger.target_total_ecd -= DEGREE_45_TO_ENCODER;
+                            launcher.trigger.target_total_ecd = launcher.trigger.motor_measure.total_ecd;
 
-//                        if(single_shoot_finish)
-//                        {
-//                            single_shoot_finish = false;
-//
-//                            launcher.trigger.target_total_ecd = launcher.trigger.motor_measure.total_ecd;
-//
-//                            launcher.trigger.target_total_ecd -= DEGREE_45_TO_ENCODER;
-//                        }
-//
+                            launcher.trigger.target_total_ecd -= DEGREE_45_TO_ENCODER;
+                        }
+
+                        error_test = launcher.trigger.motor_measure.total_ecd - launcher.trigger.target_total_ecd;
+                        test = (error_test < 2000);
+
 //                        // 判断单发完成 / 如果单发时进入堵转，下下一周期也会进入该判断
-//                        if(ABS(launcher.trigger.target_total_ecd - launcher.trigger.motor_measure.total_ecd) < 2000)
-//                        {
-//                            single_shoot_finish = true;
-//                        }
+                        if(ABS(launcher.trigger.target_total_ecd - launcher.trigger.motor_measure.total_ecd) < 2000)
+                        {
+                            single_shoot_finish = true;
+                        }
+
+                        if(test)
+                        {
+                            error_test = 0;
+                            single_shoot_finish = true;
+                        }
                     }
                     else if(vision_single == 2) // 视觉无限连发
                     {
+//                        single_shoot_finish = true;
+
                         launcher.trigger_mode = TRIGGER_CONTINUE;
 
                         launcher.trigger.target_total_ecd = launcher.trigger.motor_measure.total_ecd;
